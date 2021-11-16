ba1b7c200b54ebf104ee6b186126d373202a7943
Corey Fisher <coreyfisher2021@gmail.com>
2021-11-16 13:53:41 -0600

    commit tree 08e33ece038dce4e18393362241ffa23d1ca014a

--- main.js
+++ main.js
@@ -1,15 +1,37 @@
-// Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const error = document.getElementById("modal")
+const hearts = document.querySelectorAll("span.like-glyph");
 
-// Your JavaScript code goes here!
+error.className = "hidden";
 
+hearts.forEach(hearts => hearts.addEventListener("click", liker))
 
-
+function liker(hearts) {
+  mimicServerCall()
+  .then(() => {
+    if (hearts.target.textContent === EMPTY_HEART) {
+      hearts.target.textContent = FULL_HEART
+      hearts.target.className = "activated-heart"
+    }
+      else if (hearts.target.textContent === FULL_HEART) {
+      hearts.target.textContent = EMPTY_HEART
+      hearts.target.className = ""
+      }
+  })
+  .catch(() => {
+    error.className = "show"
+    setTimeout(() => {
+      error.className = "hidden"}, 3000
+      )
+  })
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
+
+// Defining text characters for the empty and full hearts for you to use later.
 
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
